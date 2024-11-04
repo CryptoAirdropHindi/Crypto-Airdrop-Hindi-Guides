d6da3bd556d4b63f36c2864d2d4db5921db697b0
Crypto Airdrop Hindi <124521183+CryptoAirdropHindi@users.noreply.github.com>
2024-11-04 09:38:08 +0530

    Update blockmesh.sh

--- blockmesh.sh
+++ blockmesh.sh
@@ -4,33 +4,6 @@ echo ""
 curl -s https://raw.githubusercontent.com/CryptoAirdropHindi/Crypto-Airdrop-Hindi-Guides/main/logo.sh
 sleep 2
 
-
-# Update and upgrade
-apt update && apt upgrade -y
-
-# Cleanup previous files
-rm -rf blockmesh-cli.tar.gz target
-
-# Install Docker if not installed
-if ! command -v docker &> /dev/null; then
-    echo "Installing Docker..."
-    apt-get install -y \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
-else
-    echo "Docker is already installed, skipping..."
-fi
-
-# Install Docker Compose
-echo "Installing Docker Compose..."
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
 # Create target directory for extraction
 mkdir -p target/release
 
