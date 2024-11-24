72eee587470e0d3b4fc5f96c92780872c938bb1f
Hasan Mallick <124521183+CryptoAirdropHindi@users.noreply.github.com>
2024-11-24 12:42:22 +0530

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -7,4 +7,8 @@ echo -e '██║     ██╔══██╗  ╚██╔╝  ██╔═�
 echo -e '╚██████╗██║  ██║   ██║   ██║        ██║   ╚██████╔╝██║  ██║██║██║  ██║██████╔╝██║  ██║╚██████╔╝██║     ██║  ██║██║██║ ╚████║██████╔╝██║'
 echo -e ' ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝'
 echo -e '\e[0m'
-echo -e "Join our Telegram channel: https://t.me/Crypto_airdropHM"
+echo -e "                 \033[48;2;9;10;12m Happy Cuan Airdrop \e[0m";
+echo -e "\e[0;37m Subscribe Our Telegram Channel: \e[4;35mhttps://t.me/Crypto_airdropHM/";
+echo -e "\e[0m"
+echo -e ""
+echo -e ""
